3953936185fcedadc3d378e71905883cfbabe6c0
HHVinh <vinh0986585015@gmail.com>
2024-07-07 23:07:17 +0700

    feat: exercises25.py to exercises27.py

---
+++ exercises25.py
@@ -0,0 +1,7 @@
+#Cau truc lap for
+for i in range(10):
+   #Cac lenh trong vong for duoc thuc hien nhieu lan
+   print(i)
+#Menh de else duoc thuc hien khi vong lap ket thuc binh thuong, khong bi break
+else:
+   print("Done!")
