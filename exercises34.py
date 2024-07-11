d3fe448bd7c2c8acadfeb838b9d0de1a3c2b0850
HHVinh <vinh0986585015@gmail.com>
2024-07-11 22:37:43 +0700

    feat: exercises37.py to exercises40.py

--- exercises34.py
+++ exercises34.py
@@ -2,25 +2,26 @@
 try:
     #Nhap gia tri tu ban phim
     #Ep kieu du lieu sang so nguyen
-    x = float(input())
-    n = int(input())
-   
-    #Su dung cau truc re nhanh xu ly truong hop n < 0
-    if n<0:
-        print("Vui long nhap n la so tu nhien!")
+    x = float(input("Nhập giá trị x: "))
+    n = int(input("Nhập giá trị n: "))
+
+    # Sử dụng cấu trúc rẽ nhánh xử lý trường hợp n < 0
+    if n < 0:
+        print("Vui lòng nhập n là số tự nhiên!")
     elif n == 0:
         print(1)
     else:
         ketQua = 1
         giaiThua = 1
-        #Su dung vong lap for duyet cac so tu 1 toi n
-        for i in range(1, 2*n+1):
+        # Sử dụng vòng lặp for duyệt các số từ 1 tới 2*n
+        for i in range(1, 2 * n + 1):
             giaiThua *= i
             if i % 2 == 0:
-                ketQua += pow(x, i)/giaiThua
+                ketQua += pow(x, i) / giaiThua
             else:
-                ketQua -= pow(x, i)/giaiThua 
-        print('{:.5f}'.format(ketQua))
+                ketQua -= pow(x, i) / giaiThua 
+        print(f"Kết quả là: {ketQua:.5f}")
+
         
 #Khoi lenh duoc thuc thi khi loi xay ra
 except:
