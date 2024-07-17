c79680a013d17840e38f467450012376e20d3e60
HHVinh <vinh0986585015@gmail.com>
2024-07-17 22:32:24 +0700

    feat: ex52 - list perfect numbers less than n.py

---
+++ exercises52.py
@@ -0,0 +1,31 @@
+def kiem_tra_so_hoan_thien(n):
+   tongCacUoc = 0
+   #Su dung vong lap for de duyet cac so tu 1 den n // 2 + 1
+   for i in range(1, n // 2 + 1):
+       #Kiem tra tinh chia het
+       if n % i == 0:
+           #Tinh tong cac uoc
+           tongCacUoc += i
+   if n == tongCacUoc:
+       return True
+   return False
+
+def liet_ke_so_hoan_thien(n):
+   for i in range(1, n):
+       if kiem_tra_so_hoan_thien(i):
+           print(i, end=' ')
+
+#Khoi lenh co the phat sinh loi
+try:
+   #Nhap gia tri tu ban phim
+   #Ep kieu du lieu sang so nguyen
+   n = int(input())
+  
+   #Su dung cau truc re nhanh xu ly truong hop n < 0
+   if n < 0:
+       print("Vui long nhap so tu nhien!")
+   else:
+       liet_ke_so_hoan_thien(n)
+#Khoi lenh duoc thuc thi khi loi xay ra
+except:
+   print("Dinh dang dau vao khong hop le!")
