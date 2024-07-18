c2ab7ba401f38b38ac8bfd60cde544fa1971e7f1
HHVinh <vinh0986585015@gmail.com>
2024-07-18 22:41:00 +0700

    feat: ex53 - Len anh print text.py

---
+++ exercises53.py
@@ -0,0 +1,10 @@
+def hien_thi_chuoi(s):
+   #Su dung ham len de tra ve do dai chuoi
+   print("Do dai chuoi:", len(s))
+   print("Chuoi vua nhap:", s)
+
+#Nhap gia tri tu ban phim
+s = input()
+
+#Goi ham va truyen cac tham so can thiet
+hien_thi_chuoi(s)
