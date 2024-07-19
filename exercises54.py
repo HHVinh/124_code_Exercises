7198caa52f137f66ccdb77b163ef769856ab7472
HHVinh <vinh0986585015@gmail.com>
2024-07-19 23:42:52 +0700

    feat: ex54 - concatenate 2 strings s1 and s2.py

---
+++ exercises54.py
@@ -0,0 +1,16 @@
+def chuoi_ket_qua(s1, s2):
+   #Su dung ham len de tra ve do dai chuoi
+   if len(s1) <= 5:
+       #Su dung toan tu * de tao chuoi lap lai
+       s1 = s1*3
+   if len(s2) <= 5:
+       s2 = s2*3
+   #Su dung toan tu + de noi cac chuoi
+   return s1 + s2
+
+#Nhap cac chuoi tu ban phim
+s1 = input()
+s2 = input()
+
+#Goi ham xu ly va truyen cac tham so can thiet
+print(chuoi_ket_qua(s1, s2))
